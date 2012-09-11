022749d0f773633813722a8f72c66735d96789b4
Mihai Cuibus <mihai.cuibus@gmail.com>
2012-09-11 17:55:26 +0300

    Added scripting functionalities for Linux and Windows.

---
+++ Makefile
@@ -0,0 +1,20 @@
+clean:
+	@echo Doing clean.
+	@rm -rf TaskbarJFrameGrouping/bin/*
+
+compile:
+	@echo Doing compile.
+	@cd TaskbarJFrameGrouping/src ; javac -d ../../TaskbarJFrameGrouping/bin com/example/taskbar/TaskbarJFrameGrouper.java
+
+run:
+	@echo Doing run.
+	@java -classpath `pwd`/TaskbarJFrameGrouping/lib/taskbar-lib.jar:`pwd`/TaskbarJFrameGrouping/bin com.example.taskbar.TaskbarJFrameGrouper
+
+pack_dlls:
+	@echo Doing pack_dlls.
+	@mkdir -p group-setter
+	@cp WindowGroupSetter/Debug/WindowGroupSetter.dll group-setter/WindowGroupSetter_x86.dll
+	@cp WindowGroupSetter/x64/Debug/WindowGroupSetter.dll group-setter/WindowGroupSetter_x64.dll
+	@jar -cf TaskbarJFrameGrouping/lib/taskbar-lib.jar group-setter
+	@rm -rf group-setter
+
---
+++ target.bat
@@ -0,0 +1,57 @@
+@echo off
+
+set argCount=0
+for %%x in (%*) do set /A argCount+=1
+
+if not %argCount% == 1 (
+	echo Must provide exactly one argument.
+	exit /B;
+)
+
+if "%1" == "clean" (
+	goto clean	
+)
+
+if "%1" == "compile" (
+	goto compile
+)
+
+if "%1" == "run" (
+	goto run
+)
+
+if "%1" == "pack_dlls" (
+	goto pack_dlls
+)
+
+echo Bad argument. Only 'clean', 'compile', 'run' and 'pack_dlls' are accepted.
+exit /B;
+
+:clean
+echo Doing clean.
+del /q TaskbarJFrameGrouping\bin\*.*
+rmdir /s /q TaskbarJFrameGrouping\bin\com
+exit /B;
+
+:compile
+echo Doing compile.
+cd TaskbarJFrameGrouping\src
+javac -d ..\..\TaskbarJFrameGrouping\bin com\example\taskbar\TaskbarJFrameGrouper.java
+cd ..\..
+exit /B;
+
+:run
+echo Doing run.
+java -classpath "%cd%\TaskbarJFrameGrouping\lib\taskbar-lib.jar";"%cd%\TaskbarJFrameGrouping\bin" com.example.taskbar.TaskbarJFrameGrouper
+exit /B;
+
+:pack_dlls
+echo Doing pack_dlls.
+mkdir group-setter
+copy WindowGroupSetter\Debug\WindowGroupSetter.dll group-setter\WindowGroupSetter_x86.dll
+copy WindowGroupSetter\x64\Debug\WindowGroupSetter.dll group-setter\WindowGroupSetter_x64.dll
+jar -cf TaskbarJFrameGrouping\lib\taskbar-lib.jar group-setter
+rmdir /s /q group-setter
+exit /B;
+
+
